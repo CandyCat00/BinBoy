7e79460c1347dd2237c20e4cf87a9aad88e1268c
OWBrony <97199545+OWBrony@users.noreply.github.com>
2022-06-09 08:54:22 -0600

    Create Inventory.py
    I made the inventory.

---
+++ Items/Inventory.py
@@ -0,0 +1,9 @@
+from items.Objects import objects
+from items.Cup import cup
+
+class inventory():
+    def __init__(self):
+        self.items = list()
+
+    def add_item(self, item):
+        self.items.append(item)
