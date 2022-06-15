74e6850194d8692c51530af2d89ef7a933e7e402
Catherine Cummings <87148338+CandyCat00@users.noreply.github.com>
2022-06-15 11:58:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,28 +9,76 @@ SKY = (180, 180, 255)
 GRASS = (100, 200, 100)
 GROUND = pygame.Rect(0, HEIGHT - HEIGHT/8, WIDTH, 100)
 BACKGROUND = (245, 66, 152)
+
 #set the movement speed and FPS of the game
 FPS = 60
 VEL_X = 5
-VEL_Y = 15
+VEL_Y = 20
 JUMP = False
+MOVE_L = False
+MOVE_R = False
+RUN_FRAME = 0
 
+#Level Assets
 I = 0
+LEVEL_START = 0
+END_OF_LEVEL = WIDTH * 2
 
 #Set the main character image
-BINBOY_IMAGE = pygame.image.load(os.path.join('assets', 'binboyTEST.png'))
-BINBOY_IMAGE = pygame.transform.scale(BINBOY_IMAGE, (100, 150))
+BINBOY_IDOL = pygame.image.load(os.path.join('assets', 'binboyTEST.png'))
+BINBOY_IDOL = pygame.transform.scale(BINBOY_IDOL, (100, 150))
 
+#The character images when moving right
+BINBOY_RIGHT = [None]*10
+for rightIndex in range(1, 11):
+    BINBOY_RIGHT[rightIndex - 1] = pygame.image.load(os.path.join('assets', 'binboy_right_' + str(rightIndex) + '.png'))
+    rightIndex += 1
+for rightIndex in range(1, 11):
+    BINBOY_RIGHT[rightIndex - 1] = pygame.transform.scale(BINBOY_RIGHT[rightIndex - 1], (120, 150))
+    rightIndex += 1
+
+#The character images when moving left
+BINBOY_LEFT = [None]*10
+for leftIndex in range(1, 11):
+    BINBOY_LEFT[leftIndex - 1] = pygame.image.load(os.path.join('assets', 'binboy_left_' + str(leftIndex) + '.png'))
+    leftIndex += 1
+for leftIndex in range(1, 11):
+    BINBOY_LEFT[leftIndex - 1] = pygame.transform.scale(BINBOY_LEFT[leftIndex - 1], (120, 150))
+    leftIndex += 1
+
+#Draws the character and animates him
+def draw_character(binboy):
+    global RUN_FRAME
+    if RUN_FRAME > 54: 
+        RUN_FRAME = 0
+
+    if JUMP:
+        if MOVE_R:
+            WIN.blit(BINBOY_RIGHT[4], (binboy.x, binboy.y))
+        if MOVE_L:
+            WIN.blit(BINBOY_LEFT[4], (binboy.x, binboy.y))
+        else:
+            WIN.blit(BINBOY_RIGHT[4], (binboy.x, binboy.y))
+    elif MOVE_R:
+        WIN.blit(BINBOY_RIGHT[RUN_FRAME//6], (binboy.x, binboy.y))
+        RUN_FRAME += 1
+    elif MOVE_L:
+        WIN.blit(BINBOY_LEFT[RUN_FRAME//6], (binboy.x, binboy.y))
+        RUN_FRAME += 1
+    else:
+        WIN.blit(BINBOY_IDOL, (binboy.x, binboy.y))
+        RUN_FRAME = 0
+
+#Draws everything to the screen
 def draw_window(binboy, I):
     WIN.fill(SKY)
-    pygame.draw.rect(WIN, BACKGROUND, pygame.Rect(WIDTH - I,0,WIDTH,HEIGHT))
+    pygame.draw.rect(WIN, BACKGROUND, pygame.Rect(WIDTH - I, 0, WIDTH, HEIGHT))
     pygame.draw.rect(WIN, GRASS, GROUND)
-    WIN.blit(BINBOY_IMAGE, (binboy.x, binboy.y))
+    draw_character(binboy)
     
     pygame.display.update()
 
-MOVE_L = False
-MOVE_R = False
+#tells the rest of the code what the user wants binboy to do and moves him
 def movement(binboy):
     global JUMP
     global MOVE_L
@@ -40,7 +88,7 @@ def movement(binboy):
         binboy.x -= VEL_X
         MOVE_L = True
         MOVE_R = False
-    elif keys_pressed[pygame.K_d] and binboy.x < 800: #RIGHT
+    elif keys_pressed[pygame.K_d] and binboy.x + VEL_X < WIDTH - 100: #RIGHT
         binboy.x += VEL_X
         MOVE_R = True
         MOVE_L = False
@@ -54,15 +102,14 @@ def movement(binboy):
     if JUMP:
         binboy.y -= VEL_Y
         VEL_Y -= 1
-        if VEL_Y < -15:
+        if VEL_Y < -20:
             JUMP = False
-            VEL_Y = 15
+            VEL_Y = 20
 
 
 def main():
     I = 0
-    keys_pressed = pygame.key.get_pressed()
-    binboy = pygame.Rect(400, GROUND.y - 140, 100, 150)
+    binboy = pygame.Rect(100, GROUND.y - 140, 100, 150)
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -72,13 +119,11 @@ def main():
                 run = False
 
 
-        if  MOVE_R == True:
+        if  MOVE_R and binboy.x - VEL_X > WIDTH - 500 and I != END_OF_LEVEL:
             I += VEL_X
-        if MOVE_L == True:
-            I -=VEL_X
-        if  binboy.x - VEL_X > WIDTH - 400:
             binboy.x -= VEL_X
-        if binboy.x - VEL_X < 400:
+        if MOVE_L and binboy.x - VEL_X < 300 and I != LEVEL_START:
+            I -=VEL_X
             binboy.x += VEL_X
 
         draw_window(binboy, I)
